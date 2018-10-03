40068e1076946bd059da4839485ded04514fe35e
Soniemon <arvindsundar99@gmail.com>
2018-10-02 22:40:15 -0500

    Everything except populating the modification array

--- Project2/SimBacteria.cpp
+++ Project2/SimBacteria.cpp
@@ -3,7 +3,7 @@
 
 #include <fstream>
 #include <iostream>
-#include <cctype>
+#include <string>
 
 using namespace std;
 
@@ -12,8 +12,10 @@ int fileRow(fstream&);
 int fileCol(fstream&);
 void fillArray(int**, fstream&, int, int);
 bool intVerify(string);
-//void runGenerations(int**, int**, int);
-//void rewriteFile(int**, fstream&);
+void runGenerations(int**, int**, int, int, int);
+void popMod(int**, int**, int, int);
+void nextGen(int**, int**, int, int);
+void rewriteFile(int**, fstream&, int, int);
 
 
 int main() {
@@ -24,6 +26,8 @@ int main() {
     int row, col;
     //stores how many generations of the sim will be run
     int gens;
+    //holds the input for generations
+    string holder;
     //if file is bad give output error and close program
     if(!fileCheck(simFile)) {
         cout << "FILE ERROR\nMAKE SURE A VALID FILE IS BEING USED\n";
@@ -50,6 +54,31 @@ int main() {
     lifeAry-=row;
     //fill the array
     fillArray(bacAry, simFile, row, col);
+    //close the file
+    simFile.close();
+
+    cout << "Please input how many generations you want to run (Between 1 - 10)\n";
+
+    //verify integer input
+    getline(cin, holder);
+
+    if (intVerify(holder)) {
+        gens = stoi(holder);
+    }
+
+    while(!intVerify(holder) || (gens < 1 && gens > 10)) {
+        cout << "Please enter a valid number\n\n";
+        getline(cin, holder);
+        if (intVerify(holder)) {
+            gens = stoi(holder);
+        }
+    }
+
+    //run the game
+    runGenerations(bacAry, lifeAry, gens, row, col);
+
+    //rewrite the file
+    rewriteFile(bacAry, simFile, row, col);
 
     return 0;
 }
@@ -111,6 +140,7 @@ void fillArray(int** ary, fstream& simFile, int rows, int cols) {
         *ary-=cols;
         ++ary;
     }
+    return;
     /*ary -= rows;
 
     for (int i = 0; i < rows; i++){
@@ -122,4 +152,85 @@ void fillArray(int** ary, fstream& simFile, int rows, int cols) {
         *ary-=cols;
         ++ary;
     }*/
+}
+
+bool intVerify(string input) {
+    bool isInt = true;
+    //loop through entire string to see if its an int
+    //using unsigned as not to compare signed int to unsigned
+    for (unsigned i = 0; i < input.length(); i++) {
+        if (!isdigit(input[i])) {
+            isInt = false;
+            break;
+        }
+    }
+    if (input.length() <= 0 || input.length()>2) {
+        isInt = false;
+    }
+    return isInt;
+}
+
+void runGenerations(int** ary, int** mod, int gens, int rows, int cols) {
+    //loop generation process the amount of times user input.
+    for(int i = 0; i < gens; i++) {
+        //populate the modification array
+        popMod(ary, mod, rows, cols);
+        //advance generation by 1
+        nextGen(ary, mod, rows, cols);
+    }
+}
+
+void popMod(int** ary, int** mod, int rows, int cols) {
+
+
+}
+
+void nextGen(int** ary, int** mod, int rows, int cols) {
+    //based on the changes made to the modified array change main array
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            **ary = **mod;
+            ++*ary;
+            ++*mod;
+        }
+        *ary-=cols;
+        *mod-=cols;
+        ++ary;
+        ++mod;
+    }
+}
+
+void rewriteFile(int** ary, fstream& simFile, int rows, int cols) {
+    //open the file with out and truncate tags
+    simFile.open("simbac.txt", ios::out | ios::trunc);
+
+    //parse through entire array
+    //if 0 ' ' if 1 '*'
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            if(**ary){
+                cout << '*';
+            } else {
+                cout << ' ';
+            }
+            ++*ary;
+        }
+        cout << endl;
+        *ary-=cols;
+        ++ary;
+    }
+    //write to file
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            if(**ary){
+                simFile.put('*');
+            } else {
+                simFile.put(' ');
+            }
+            ++*ary;
+        }
+        simFile.put('\n');
+        *ary-=cols;
+        ++ary;
+    }
 }
